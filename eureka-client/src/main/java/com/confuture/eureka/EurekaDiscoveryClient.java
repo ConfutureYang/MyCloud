626106924baa587b425ba3ccd7c2e40df6ebb6f6
yang windows <yang@windows.com>
2020-07-04 21:53:12 +0800

    'update'

--- eureka-client/src/main/java/com/confuture/eureka/EurekaDiscoveryClient.java
+++ eureka-client/src/main/java/com/confuture/eureka/EurekaDiscoveryClient.java
@@ -1,14 +1,24 @@
 package com.confuture.eureka;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaDiscoveryClient {
     public static void main(String[] args) {
         SpringApplication.run(EurekaDiscoveryClient.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
     }
 }
